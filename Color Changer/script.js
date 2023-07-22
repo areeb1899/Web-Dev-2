e3e950e78d4ae93ea4cfab7e59e427e7088930f9
Areeb Ahmed <131575841+areeb1899@users.noreply.github.com>
2023-07-22 12:58:28 +0530

    Add files via upload

---
+++ Color Changer/script.js
@@ -0,0 +1,7 @@
+let index = 0;
+
+function changeColor() {
+    let color=Math.floor(Math.random()*0xffffff).toString(16);
+    document.getElementsByTagName("body")[0].style.background = `#${color}`
+    
+}
