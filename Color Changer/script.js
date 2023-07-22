c2422e11c25d20a159967ba3596af3a970b0719c
Areeb Ahmed <131575841+areeb1899@users.noreply.github.com>
2023-07-22 12:58:56 +0530

    Delete Color Changer directory

--- Color Changer/script.js
+++
@@ -1,7 +0,0 @@
-let index = 0;
-
-function changeColor() {
-    let color=Math.floor(Math.random()*0xffffff).toString(16);
-    document.getElementsByTagName("body")[0].style.background = `#${color}`
-    
-}
