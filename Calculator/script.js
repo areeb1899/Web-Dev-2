5845782246a188551200e5a1cbb0bbd14ddceb1f
Areeb Ahmed <131575841+areeb1899@users.noreply.github.com>
2023-07-30 00:55:12 +0530

    script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,41 +1,40 @@
-let num1 = prompt('Enter the first number');
-num1 = Number.parseInt(num1);
-let num2 = prompt('Enter the second number');
-num2 = Number.parseInt(num2);
-document.getElementById("number1").textContent ="First Number: " +num1;
-document.getElementById("number2").textContent ="Second Number: "+ num2;
 
-
-
-let resultCalc = document.getElementById("result");
 
 function add() {
-    let result = num1 + num2
-    resultCalc.textContent = "Result: " + result;
-
+    const input1=document.getElementById('input1').value;
+    const input2=document.getElementById('input2').value;
+    let result = Number(input1) + Number(input2);
+    document.getElementById('showResult').textContent="Result: " + result;;
 
 
 }
 
 function substract() {
-    let result = num1 - num2
-    resultCalc.textContent = "Result: " + result;
+    const input1=document.getElementById('input1').value;
+    const input2=document.getElementById('input2').value;
+    let result = Number(input1) - Number(input2);
+    document.getElementById('showResult').textContent="Result: " + result;;
 }
 
 function multiply() {
-    let result = num1 * num2
-    resultCalc.textContent = "Result: " + result;
+    const input1=document.getElementById('input1').value;
+    const input2=document.getElementById('input2').value;
+    let result = Number(input1) * Number(input2);
+    document.getElementById('showResult').textContent="Result: " + result;;
 }
 
 function divide() {
-    let result = (Math.round(num1) /(num2)).toFixed(2)
-    resultCalc.textContent = "Result: " + result;
+    const input1=document.getElementById('input1').value;
+    const input2=document.getElementById('input2').value;
+    let result = Number((input1) / (input2)).toFixed(2);
+    document.getElementById('showResult').textContent="Result: " + result;;
 }
 
 function mod(){
-   let result= num1%num2;
-   resultCalc.textContent = "Result: " + result;
-// console.log(result);
+    const input1=document.getElementById('input1').value;
+    const input2=document.getElementById('input2').value;
+    let result = Number(input1) % Number(input2);
+    document.getElementById('showResult').textContent="Result: " + result;;
 
 }
 function reset() {
